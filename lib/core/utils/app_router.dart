ddca4fef59336d42ea72ca09ad1b07323fb59baa
car <you@example.com>
2023-09-12 14:26:38 +0300

    app_route

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:car_rental/features/splash/view/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../features/onboarding/view/onboardingview.dart';
+
+abstract class AppRouter {
+  static const onBoardingView = "/onboardingview";
+
+  static final router = GoRouter(routes: [
+    GoRoute(
+      path: "/",
+      builder: (context, state) => const SplashView(),
+    ),
+    GoRoute(
+      path: "onboardingview",
+      builder: (context, state) => const OnBoardingView(),
+    )
+  ]);
+}
--- lib/features/splash/view/widget/splash_view_body.dart
+++ lib/features/splash/view/widget/splash_view_body.dart
@@ -1,7 +1,19 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class SplashViweBody extends StatelessWidget {
-  const SplashViweBody({super.key});
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +22,12 @@ class SplashViweBody extends StatelessWidget {
       body: Container(
         height: size.height,
         width: size.width,
-        decoration: const BoxDecoration(),
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(
+                  "assets/images/splash.png",
+                ),
+                fit: BoxFit.fill)),
       ),
     );
   }
